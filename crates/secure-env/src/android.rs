013d6b99f2fee60e87f1a64b070da42b1d3939ec
Berend Sliedrecht <blu3beri@proton.me>
2023-07-25 21:35:22 +0200

    android: create EC key via JNI
    Signed-off-by: Berend Sliedrecht <blu3beri@proton.me>

--- crates/secure-env/src/android.rs
+++ crates/secure-env/src/android.rs
@@ -1,13 +1,14 @@
 use jni::{
-    objects::JValue,
+    objects::{JClass, JString, JValue},
+    signature::ReturnType,
     sys::{self, jint},
-    InitArgsBuilder, JNIVersion, JavaVM,
+    AttachGuard, InitArgsBuilder, JNIVersion, JavaVM,
 };
 
 use crate::common_hsm::CommonHsm;
 
 #[derive(Debug)]
-pub struct Android(JavaVM);
+pub struct Android;
 
 impl CommonHsm for Android {
     fn sign(&self, msg: &[u8]) -> Vec<u8> {
@@ -21,6 +22,64 @@ impl CommonHsm for Android {
 
 impl Android {
     pub fn new(vm: JavaVM) -> Self {
-        Self(vm)
+        let mut env = vm.attach_current_thread().unwrap();
+        let b = env.new_string("EC").unwrap();
+        let p = JValue::Object(&b);
+        let cls = env.find_class("java/security/KeyPairGenerator").unwrap();
+        let res = env
+            .call_static_method(
+                cls,
+                "getInstance",
+                "(Ljava/lang/String;)Ljava/security/KeyPairGenerator;",
+                &[p],
+            )
+            .unwrap();
+        let res = res.l().unwrap();
+
+        let res = env
+            .call_method(
+                &res,
+                "generateKeyPair",
+                "()Ljava/security/KeyPair;",
+                &[]
+            )
+            .unwrap();
+        let res = res.l().unwrap();
+
+        let res = env
+            .call_method(
+                &res,
+                "getPublic",
+                "()Ljava/security/PublicKey;",
+                &[]
+            )
+            .unwrap();
+        let res = res.l().unwrap();
+
+        let res = env
+            .call_method(
+                &res,
+                "getAlgorithm",
+                "()Ljava/lang/String;",
+                &[]
+            )
+            .unwrap();
+        let res = JString::from(res.l().unwrap());
+        let res = env.get_string(&res).unwrap();
+        let res = res.to_str().unwrap();
+
+        println!("{res:?}");
+
+        Self
     }
 }
+
+// println!("key_pair_generator: {key_pair_generator:?}");
+//let method = env
+//    .get_static_method_id(
+//        "java/security/KeyPairGenerator",
+//        "getInstance",
+//        "(Ljava/lang/String;)Ljava/security/KeyPairGenerator",
+//    )
+//    .unwrap();
+// println!("method: {method:?}");
