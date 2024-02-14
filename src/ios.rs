4eb2d06846516a586bd32323f3ec42af6bd6dc97
Berend Sliedrecht <sliedrecht@berend.io>
2024-02-14 11:47:36 +0100

    feat(ios): get public key from SecKey
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- src/ios.rs
+++ src/ios.rs
@@ -2,6 +2,7 @@ use crate::{
     error::{SecureEnvError, SecureEnvResult},
     KeyOps, SecureEnvironmentOps,
 };
+use p256::elliptic_curve::group::GroupEncoding;
 use security_framework::key::{GenerateKeyOptions, KeyType, SecKey, Token};
 
 /// Unit struct that can be used to create and get keypairs by id
@@ -47,12 +48,47 @@ impl SecureEnvironmentOps<Key> for SecureEnvironment {
     }
 }
 
+/// Key structure which allows for signing and retrieval of the public key
+///
+/// # Examples
+///
+/// ```
+/// use secure_env::{SecureEnvironment, SecureEnvironmentOps, Key, KeyOps};
+///
+/// let key = SecureEnvironment::generate_keypair("documentation-public-key-token").unwrap();
+/// let public_key_bytes = key.get_public_key().unwrap();
+/// ```
+///
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Key(SecKey);
 
 impl KeyOps for Key {
     fn get_public_key(&self) -> SecureEnvResult<Vec<u8>> {
-        todo!()
+        // Retrieve the internal representation of the public key of the `SecKey`
+        let public_key = self
+            .0
+            .public_key()
+            .ok_or(SecureEnvError::UnableToGetPublicKey(Some(
+                "No public key reference found on the internal `SecKey`".to_owned(),
+            )))?;
+
+        // Convert the public key reference to the `sec1` format in bytes
+        let sec1_bytes = public_key
+            .external_representation()
+            .ok_or(SecureEnvError::UnableToGetPublicKey(Some(
+                "Could not create an external representation for the public key on the `SecKey`"
+                    .to_owned(),
+            )))?
+            .to_vec();
+
+        // Instantiate a P256 public key from the `sec1` bytes
+        let public_key = p256::PublicKey::from_sec1_bytes(&sec1_bytes)
+            .map_err(|e| SecureEnvError::UnableToGetPublicKey(Some(e.to_string())))?;
+
+        // Get the affine point of the public key and convert this into a byte representation
+        let public_key = public_key.as_affine().to_bytes().to_vec();
+
+        Ok(public_key)
     }
 
     fn sign(&self, msg: &[u8]) -> SecureEnvResult<Vec<u8>> {
@@ -70,5 +106,13 @@ mod test {
     fn generate_key_pair() {
         let key = SecureEnvironment::generate_keypair("my-test-key").unwrap();
         assert!(!addr_of!(key).is_null());
+    }
+
+    #[test]
+    fn get_public_key() {
+        let key = SecureEnvironment::generate_keypair("my-test-public-key").unwrap();
+        let public_key_bytes = key.get_public_key().unwrap();
+
+        assert_eq!(public_key_bytes.len(), 33);
     }
 }
