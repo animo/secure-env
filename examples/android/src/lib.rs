ff2cf2d90e2a0e698fb0b18fd0bb150fa94163c3
Berend Sliedrecht <sliedrecht@berend.io>
2024-02-16 14:06:02 +0100

    build(ci): better android finish test check
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- examples/android/src/lib.rs
+++ examples/android/src/lib.rs
@@ -1,26 +1,14 @@
 use android_activity::AndroidApp;
-use secure_env::{
-    error::SecureEnvResult, key::KeyOps, secure_environment::SecureEnvironmentOps,
-    SecureEnvironment,
-};
-
-fn main() -> SecureEnvResult<()> {
-    let k = SecureEnvironment::generate_keypair("some-id")?;
-    let n_k = SecureEnvironment::get_keypair_by_id("some-id")?;
-
-    let k = k.get_public_key()?;
-    let n_k = n_k.get_public_key()?;
-
-    assert_eq!(k, n_k);
-    println!("Created and got key and they are equal");
-
-    Ok(())
-}
+use secure_env::{key::KeyOps, secure_environment::SecureEnvironmentOps, SecureEnvironment};
 
 #[no_mangle]
 fn android_main(_app: AndroidApp) {
-    match main() {
-        Ok(_) => println!("Success!"),
-        Err(e) => eprintln!("Error!: {e:?}"),
-    }
+    let k = SecureEnvironment::generate_keypair("some-id").unwrap();
+    let n_k = SecureEnvironment::get_keypair_by_id("some-id").unwrap();
+
+    let k = k.get_public_key().unwrap();
+    let n_k = n_k.get_public_key().unwrap();
+
+    assert_eq!(k, n_k);
+    println!("Created and got key and they are equal");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(concat_idents)]
+#![feature(concat_idents, pointer_is_aligned)]
 
 #[cfg(not(any(target_os = "android", target_os = "ios")))]
 compile_error!("Only Android and iOS are supported targets");
