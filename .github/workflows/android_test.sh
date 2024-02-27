e727a365656e13111cf9b1632688c811de12dfa4
Berend Sliedrecht <sliedrecht@berend.io>
2024-02-27 16:51:37 +0100

    build(android): combine bash statements
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- .github/workflows/android_test.sh
+++ .github/workflows/android_test.sh
@@ -13,17 +13,18 @@ LOG=$(adb logcat -d RustStdoutStderr:D '*:S')
 if echo $LOG | grep 'RustStdoutStderr';
 then
     echo "App running"
+    MSG=$(echo $LOG | grep 'panicked')
+    echo $MSG
+    if [ -z "$MSG" ]; then
+      echo "::success:: All tests passed!"
+      exit 0
+    else
+      echo "::error:: Rust panicked! Tests failed. Logs will be uploaded"
+      echo $LOG >> ~/logcat.log
+      exit 1
+    fi
 else
     echo "::error:: App not running"
     exit 1
 fi
 
-MSG=$(echo $LOG | grep 'panicked')
-if [ -z "$MSG" ]; then
-  echo "::success:: All tests passed!"
-  exit 0
-else
-  echo "::error:: Rust panicked! Tests failed. Logs will be uploaded"
-  echo $LOG >> ~/logcat.log
-  exit 1
-fi
